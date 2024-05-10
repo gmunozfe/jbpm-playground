acfd856f922aa11537e7e8e832da561a6e9a05a8
gmunozfe <gmunozfe@redhat.com>
2024-05-10 12:39:58 +0200

    tune springboot test to use mysql

--- jbpm-system-test/jbpm-springboot-mssql-test/src/test/java/org/kie/server/springboot/samples/KieServerMSSQLTest.java
+++ jbpm-system-test/jbpm-springboot-mssql-test/src/test/java/org/kie/server/springboot/samples/KieServerMSSQLTest.java
@@ -39,6 +39,7 @@ import org.kie.server.client.KieServicesConfiguration;
 import org.kie.server.client.KieServicesFactory;
 import org.kie.server.client.ProcessServicesClient;
 import org.kie.server.client.QueryServicesClient;
+import org.kie.server.client.UserTaskServicesClient;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,7 +52,22 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import org.testcontainers.containers.MSSQLServerContainer;
+import org.assertj.core.api.Assertions;
+import org.kie.server.api.model.definition.QueryDefinition;
+import org.kie.server.api.model.definition.TaskField;
+import org.kie.server.api.model.definition.TaskQueryFilterSpec;
+import org.kie.server.api.model.instance.JobRequestInstance;
+import org.kie.server.api.model.instance.TaskInstance;
+import org.kie.server.api.model.instance.TaskSummary;
+import org.kie.server.api.util.TaskQueryFilterSpecBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.containers.MySQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -64,6 +80,10 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER
 public class KieServerMSSQLTest {
 
     private static final Logger logger = LoggerFactory.getLogger(KieServerMSSQLTest.class);
+    
+    private static final String PROCESS_ID_USERTASK = "definition-project.usertask";   
+    private static final String QUERY_NAME = "taskInstancesQuery";
+    private static final String TASK_QUERY = "select ti.* from AuditTaskImpl ti";
 
     static final String PATH = "src/test/resources/kjars/";
     
@@ -83,13 +103,17 @@ public class KieServerMSSQLTest {
 
     private KieServicesClient kieServicesClient;
 
-    static final MSSQLServerContainer<?> MSSQL_CONTAINER;
+    //static final MSSQLServerContainer<?> MSSQL_CONTAINER;
+    static final MySQLContainer<?> MSSQL_CONTAINER;
+    
+    
     
     static {
-        MSSQL_CONTAINER = new MSSQLServerContainer<>(System.getProperty("org.kie.samples.image.sqlserver","mcr.microsoft.com/mssql/server:2019-latest"))
-                                 .acceptLicense();
+        //MSSQL_CONTAINER = new MSSQLServerContainer<>(System.getProperty("org.kie.samples.image.sqlserver","mcr.microsoft.com/mssql/server:2019-latest"))
+        //                         .acceptLicense();
         
-        MSSQL_CONTAINER.withInitScript("sql/sqlserver-springboot-jbpm-schema.sql");
+    	MSSQL_CONTAINER = new MySQLContainer<>("mysql:8.0.24");
+        //MSSQL_CONTAINER.withInitScript("sql/sqlserver-springboot-jbpm-schema.sql");
         MSSQL_CONTAINER.start();
     }
     
@@ -126,10 +150,22 @@ public class KieServerMSSQLTest {
         KieContainerResource resource = new KieContainerResource(CONTAINER_ID, releaseId);
         resource.setContainerAlias(ALIAS);
         kieServicesClient.createContainer(CONTAINER_ID, resource);
+    
+        QueryDefinition query = new QueryDefinition();
+        query.setName(QUERY_NAME);
+        query.setSource(System.getProperty("org.kie.server.persistence.ds", "jdbc/jbpm-ds"));
+        query.setExpression(TASK_QUERY);
+        query.setTarget("CUSTOM");
+        
+        QueryServicesClient queryClient = kieServicesClient.getServicesClient(QueryServicesClient.class);
+        queryClient.registerQuery(query);
     }
 
+        
     @AfterEach
     public void cleanup() {
+    	QueryServicesClient queryClient = kieServicesClient.getServicesClient(QueryServicesClient.class);
+    	queryClient.unregisterQuery(QUERY_NAME);
         if (kieServicesClient != null) {
             ServiceResponse<Void> response = kieServicesClient.disposeContainer(CONTAINER_ID);
             logger.info("Container {} disposed with response - {}", CONTAINER_ID, response.getMsg());
@@ -167,6 +203,83 @@ public class KieServerMSSQLTest {
         ProcessInstance processInstance = queryClient.findProcessInstanceById(processInstanceId);
         assertNotNull(processInstance);
         assertEquals(3, processInstance.getState().intValue());
+    }
+    
+    @Test
+    public void testFindTaskWithNameEqualsToFilter() throws Exception {
+    	UserTaskServicesClient taskClient = kieServicesClient.getServicesClient(UserTaskServicesClient.class);
+    	ProcessServicesClient processClient = kieServicesClient.getServicesClient(ProcessServicesClient.class);
+        Long processInstanceId = processClient.startProcess( CONTAINER_ID, "definition-project.usertask" );
+        Assertions.assertThat( processInstanceId ).isNotNull();
+
+       /* List<TaskSummary> tasks = taskClient.findTasksAssignedAsPotentialOwner( "yoda",
+                                                                                0,
+                                                                                10 );*/
+        
+        List<TaskSummary> tasks = taskClient.findTasksByStatusByProcessInstanceId(processInstanceId, null, 0, 10);
+
+        System.out.println("\t######### Tasks: " +tasks);
+        
+        Assertions.assertThat( tasks ).isNotEmpty();
+        TaskSummary task = tasks.get( 0 );
+        testFindTaskInstanceWithSearchService( createQueryFilterEqualsTo( TaskField.NAME,
+        		                                                          "First task" ),
+                                               task.getId() );
+    }               
+    
+    private void testFindTaskInstanceWithSearchService( TaskQueryFilterSpec filter,
+            Long taskInstanceId ) {
+            List<Long> resultsIds = new ArrayList<>();
+            QueryServicesClient queryClient = kieServicesClient.getServicesClient(QueryServicesClient.class);
+        	
+            List<TaskInstance> results = queryClient.findHumanTasksWithFilters( QUERY_NAME, 
+                                            filter,
+                                            0,
+                                            100 );
+            for ( TaskInstance res : results ) {
+            resultsIds.add( res.getId() );
+            
+            Assertions.assertThat(res.getInputData()).isNullOrEmpty();
+            Assertions.assertThat(res.getOutputData()).isNullOrEmpty();
+            }
+            
+            Assertions.assertThat( results ).isNotNull();
+            Assertions.assertThat( results ).isNotEmpty();
+            
+            System.out.println("@@@ resultsIds: "+resultsIds);
+            Assertions.assertThat( resultsIds ).contains( taskInstanceId );
+            }
+            
+    private TaskQueryFilterSpec createQueryFilterEqualsTo( TaskField taskField,
+                           Comparable<?> equalsTo ) {
+            return new TaskQueryFilterSpecBuilder().equalsTo( taskField,
+                          equalsTo ).get();
+    }
+            
+    private TaskQueryFilterSpec createQueryFilterGreaterThan( TaskField taskField,
+                              Comparable<?> greaterThan ) {
+            return new TaskQueryFilterSpecBuilder().greaterThan( taskField,
+                             greaterThan ).get();
+    }
+            
+    private TaskQueryFilterSpec createQueryFilterGreaterThanAndEqualsTo( TaskField taskField,
+                                         Comparable<?> greaterThan,
+                                         Comparable<?> equalsTo ) {
+            return new TaskQueryFilterSpecBuilder().greaterThan( taskField,
+                             greaterThan ).equalsTo( taskField,
+                                                     equalsTo ).get();
+    }
+            
+    private TaskQueryFilterSpec createQueryFilterGreaterThanOrEqualsTo( TaskField taskField,
+                                        Comparable<?> equalsTo ) {
+            return new TaskQueryFilterSpecBuilder().greaterOrEqualTo( taskField,
+                                  equalsTo ).get();
+            }
+            
+            private TaskQueryFilterSpec createQueryFilterAndEqualsTo( Map<TaskField, Comparable<?>> filterProperties ) {
+            TaskQueryFilterSpecBuilder result = new TaskQueryFilterSpecBuilder();
+            filterProperties.forEach( result::equalsTo );
+            return result.get();
     }
 
 }
